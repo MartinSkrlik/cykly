175f2d5fea46dd785ae6e0695cb1a64d79bdc84a
martin s <Martinskrlik33@gmail.com>
2021-05-16 12:08:38 +0200

    Add project files.

---
+++ cykly/cyklus_for.cpp
@@ -0,0 +1,21 @@
+// textin4.cpp -- ètení znakù pomocí cin.get()
+#include <iostream>
+int main()
+{
+	using namespace std;
+
+	int m, j, vysledok = 0;
+
+	cout << "zadaj cele cislo do programu";
+	cin >> m;
+	cout << "zadaj druhe cele cislo do programu";
+	cin >> j;
+
+	for (m; m <= 9; m++)
+	{
+		vysledok += m;
+
+	}
+	cout << "sucet vsetkych celych cisel medzi 2 a 9 je " << vysledok << endl;
+	return 0;
+}
